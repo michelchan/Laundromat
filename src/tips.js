c36cd79be3dc26baca6c10168fd20a7d104f4d6b
Michelle <michelchan421@gmail.com>
2016-08-11 22:29:29 -0400

    Added tips

---
+++ src/tips.js
@@ -0,0 +1,15 @@
+/* All information was found through Google. I used various sites such as Reddit and Good House Keeping, as well as personal knowledge. */
+
+module.exports = {
+	"Pretreat stains to keep them from setting.",
+	"Hang up clothes immediately after dryingto prevent wrinkles",
+	"Whites in warm, colors on cold.",
+	"Don't overload the washer and dryer",
+	"Clean the lint trap after every dryer load.",
+	"Hang dry clothes that you are afriad of shrinking",
+	"Consider a lingerie bag if you don't want to hand wash bras",
+	"Don't forget to check pockets",
+	"Turn clothes inside out before washing.",
+	"Zip up zippers to prevent damage to the zipper and other clothing",
+	"Separate clothes by into different hampers for an easier laundry day. For example, one for colors and one for whites",
+};
